dca2fc3842ae7bfb12f049131ba61f03ace49db9
ladyada <limor@ladyada.net>
2014-08-18 13:27:19 -0400

    addeding debug and spi bitmap example + pic

--- Adafruit_HX8357.cpp
+++ Adafruit_HX8357.cpp
@@ -257,8 +257,8 @@ void Adafruit_HX8357::begin(uint8_t type) {
     delay(300);
     // setRGB which also enables SDO
     writecommand(HX8357_SETRGB); 
-//    writedata(0x80);  //enable SDO pin!
-    writedata(0x00);  //disable SDO pin!
+    writedata(0x80);  //enable SDO pin!
+//    writedata(0x00);  //disable SDO pin!
     writedata(0x0);
     writedata(0x06);
     writedata(0x06);
--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -1,26 +1,47 @@
-#define RST 8
-#define DC 9
-#define CS 10
+/***************************************************
+  This is our library for the Adafruit HX8357D Breakout
+  ----> http://www.adafruit.com/products/2050
+
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
 
 #include <SPI.h>
 #include "Adafruit_GFX.h"
 #include "Adafruit_HX8357.h"
 
-// For the Adafruit shield, these are the default.
-#define TFT_RST 8
-#define TFT_DC 9
+// These are 'flexible' lines that can be changed
 #define TFT_CS 10
+#define TFT_DC 9
+#define TFT_RST 8 // RST can be set to -1 if you tie it to Arduino's reset
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
-
-
 
 void setup() {
   Serial.begin(9600);
   Serial.println("HX8357D Test!"); 
 
   tft.begin(HX8357D);
+
+  // read diagnostics (optional but can help debug problems)
+  uint8_t x = tft.readcommand8(HX8357_RDPOWMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDCOLMOD);
+  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDIM);
+  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(HX8357_RDDSDR);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
   
   Serial.println(F("Benchmark                Time (microseconds)"));
 
