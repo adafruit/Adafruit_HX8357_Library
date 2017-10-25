2107afa5db5db41483f5a407cff1d59129599037
ladyada <limor@ladyada.net>
2017-10-25 16:47:35 -0400

    demodemo

---
+++ examples/touchpaint_featherwing/touchpaint_featherwing.ino
@@ -0,0 +1,174 @@
+/***************************************************
+  This is our library for the Adafruit HX8357D Featherwing
+  ----> http://www.adafruit.com/products/2050
+
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_HX8357.h>
+#include <Adafruit_STMPE610.h>
+
+#ifdef ESP8266
+   #define STMPE_CS 16
+   #define TFT_CS   0
+   #define TFT_DC   15
+   #define SD_CS    2
+#endif
+#ifdef ESP32
+   #define STMPE_CS 32
+   #define TFT_CS   15
+   #define TFT_DC   33
+   #define SD_CS    14
+#endif
+#ifdef __AVR_ATmega32U4__
+   #define STMPE_CS 6
+   #define TFT_CS   9
+   #define TFT_DC   10
+   #define SD_CS    5
+#endif
+#ifdef ARDUINO_SAMD_FEATHER_M0
+   #define STMPE_CS 6
+   #define TFT_CS   9
+   #define TFT_DC   10
+   #define SD_CS    5
+#endif
+#ifdef TEENSYDUINO
+   #define TFT_DC   10
+   #define TFT_CS   4
+   #define STMPE_CS 3
+   #define SD_CS    8
+#endif
+#ifdef ARDUINO_STM32_FEATHER
+   #define TFT_DC   PB4
+   #define TFT_CS   PA15
+   #define STMPE_CS PC7
+   #define SD_CS    PC5
+#endif
+#ifdef ARDUINO_FEATHER52
+   #define STMPE_CS 30
+   #define TFT_CS   13
+   #define TFT_DC   11
+   #define SD_CS    27
+#endif
+
+#define TFT_RST -1
+
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 3800
+#define TS_MAXX 100
+#define TS_MINY 100
+#define TS_MAXY 3750
+
+// Size of the color selection boxes and the paintbrush size
+#define BOXSIZE 40
+#define PENRADIUS 3
+int oldcolor, currentcolor;
+
+
+void setup() {
+  Serial.begin(115200);
+  //while (!Serial) delay(10);
+  
+  Serial.println("HX8357D Featherwing touch test!"); 
+  
+  if (!ts.begin()) {
+    Serial.println("Couldn't start touchscreen controller");
+    while (1);
+  }
+  Serial.println("Touchscreen started");
+  
+  tft.begin(HX8357D);
+  tft.fillScreen(HX8357_BLACK);
+    // make the color selection boxes
+  tft.fillRect(0, 0, BOXSIZE, BOXSIZE, HX8357_RED);
+  tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, HX8357_YELLOW);
+  tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, HX8357_GREEN);
+  tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, HX8357_CYAN);
+  tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, HX8357_BLUE);
+  tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, HX8357_MAGENTA);
+  tft.fillRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, HX8357_BLACK);
+  tft.fillRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+   
+  // select the current color 'red'
+  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+  currentcolor = HX8357_RED;
+}
+
+
+void loop(void) {
+  // Retrieve a point  
+  TS_Point p = ts.getPoint();
+  
+ // Serial.print("X = "); Serial.print(p.x); Serial.print("\tY = "); Serial.print(p.y);  Serial.print("\tPressure = "); Serial.println(p.z); 
+ 
+  // Scale from ~0->4000 to tft.width using the calibration #'s
+  p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+
+  if (p.y < BOXSIZE) {
+     oldcolor = currentcolor;
+
+     if (p.x < BOXSIZE) { 
+       currentcolor = HX8357_RED; 
+       tft.drawRect(0, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*2) {
+       currentcolor = HX8357_YELLOW;
+       tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*3) {
+       currentcolor = HX8357_GREEN;
+       tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*4) {
+       currentcolor = HX8357_CYAN;
+       tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*5) {
+       currentcolor = HX8357_BLUE;
+       tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*6) {
+       currentcolor = HX8357_MAGENTA;
+       tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     } else if (p.x < BOXSIZE*7) {
+       currentcolor = HX8357_WHITE;
+       tft.drawRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, HX8357_RED);
+     } else if (p.x < BOXSIZE*8) {
+       currentcolor = HX8357_BLACK;
+       tft.drawRect(BOXSIZE*7, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+     }
+
+
+     if (oldcolor != currentcolor) {
+        if (oldcolor == HX8357_RED) 
+          tft.fillRect(0, 0, BOXSIZE, BOXSIZE, HX8357_RED);
+        if (oldcolor == HX8357_YELLOW) 
+          tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, HX8357_YELLOW);
+        if (oldcolor == HX8357_GREEN) 
+          tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, HX8357_GREEN);
+        if (oldcolor == HX8357_CYAN) 
+          tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, HX8357_CYAN);
+        if (oldcolor == HX8357_BLUE) 
+          tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, HX8357_BLUE);
+        if (oldcolor == HX8357_MAGENTA) 
+          tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, HX8357_MAGENTA);
+        if (oldcolor == HX8357_WHITE) 
+          tft.fillRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, HX8357_WHITE);
+        if (oldcolor == HX8357_BLACK) 
+          tft.fillRect(BOXSIZE*7, 0, BOXSIZE, BOXSIZE, HX8357_BLACK);
+     }
+  }
+
+  if (((p.y-PENRADIUS) > 0) && ((p.y+PENRADIUS) < tft.height())) {
+    tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
+  }
+}
